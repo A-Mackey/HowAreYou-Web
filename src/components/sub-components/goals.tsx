2d85ab0d35ffb8f00d98a11dc677e06df552c8b9
A-Mackey <57772122+A-Mackey@users.noreply.github.com>
2023-01-23 13:07:03 -0800

    Working on profile page and adding/removing user goals

--- src/components/sub-components/goals.tsx
+++ src/components/sub-components/goals.tsx
@@ -1,34 +1,94 @@
+import { useEffect, useState } from "react";
+import { APIProvider } from "../../context/APIContext";
+import { UserAuth } from "../../context/AuthContext";
+import Loading from "./loading";
+
 type GoalsProps = {
   onClick?: (index: number) => void;
   goals: string[];
   selected?: boolean[];
+  editable?: boolean;
 };
 
 function Goals(props: GoalsProps) {
+  const { user } = UserAuth();
+  const { setUserGoals, getUserGoals } = APIProvider();
+
+  const [goals, setGoals] = useState<string[]>([]);
+  const [addedGoal, setAddedGoal] = useState<string>("");
+
+  useEffect(() => {
+    console.log("Mounted Goals");
+    hydrateGoals();
+  }, []);
+
+  const hydrateGoals = async () => {
+    setGoals(await getUserGoals());
+  };
+
+  const addGoal = () => {
+    setUserGoals([...goals, addedGoal]);
+    setGoals([...goals, addedGoal]);
+  };
+
+  const removeGoal = (index: number) => {
+    const newGoals = goals.filter((goal, i) => i !== index);
+    setUserGoals(newGoals);
+    setGoals(newGoals);
+  };
+
   return (
     <div className="goals-container">
-      {props.goals.map((goal: string, index: number) => (
-        <div className="goal-single" key={index}>
-          <button
-            className="goal-row"
-            onClick={() => {
-              if (props.onClick !== undefined) props.onClick(index);
-            }}>
-            <div className="goal-left">
-              <div
-                className={
-                  props.selected !== undefined
-                    ? props.selected[index]
-                      ? "goal-bullet-filled"
+      {user === null ? (
+        <Loading />
+      ) : (
+        goals.map((goal: string, index: number) => (
+          <div className="goal-single" key={index}>
+            <button
+              className="goal-row"
+              onClick={() => {
+                if (props.onClick !== undefined) props.onClick(index);
+              }}>
+              <div className="goal-left">
+                <div
+                  className={
+                    props.selected !== undefined
+                      ? props.selected[index]
+                        ? "goal-bullet-filled"
+                        : "goal-bullet"
                       : "goal-bullet"
-                    : "goal-bullet"
-                }
-              />
-            </div>
-            <div className="goal-goal">{goal}</div>
+                  }
+                />
+              </div>
+              <div className="goal-center">
+                <div className="goal-goal">{goal}</div>
+              </div>
+              <div className="goal-right">
+                <button
+                  onClick={() => removeGoal(index)}
+                  className="goal-remove-button">
+                  <img src="/icons/X.svg" alt="X" />
+                </button>
+              </div>
+            </button>
+          </div>
+        ))
+      )}
+
+      {props.editable !== undefined && props.editable && (
+        <div className="goal-add-section">
+          <button className="goal-add-button" onClick={() => addGoal()}>
+            +
           </button>
+          <input
+            type="text"
+            className="goal-input"
+            onChange={(n) => {
+              setAddedGoal(n.target.value);
+            }}
+          />
         </div>
-      ))}
+      )}
     </div>
   );
 }
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import Goals from "../components/sub-components/goals";
+import Loading from "../components/sub-components/loading";
+import { APIProvider } from "../context/APIContext";
 import { UserAuth } from "../context/AuthContext";
 
 function Profile(_props: any) {
+  const { getUserGoals } = APIProvider();
   const { user } = UserAuth();
+
+  const [goals, setGoals] = useState<string[]>([]);
+
+  useEffect(() => {
+    hydrateGoals();
+  }, [user]);
+
+  const hydrateGoals = async () => {
+    setGoals(await getUserGoals());
+  };
 
   return (
     <div className="profile-container">
@@ -13,10 +28,44 @@ function Profile(_props: any) {
 
         <div className="profile-content">
           <div className="profile-content-inputs">
-            <textarea className="profile-textarea" />
+            <div className="profile-input-with-header">
+              <h3>First Name</h3>
+              <input
+                type="text"
+                className="profile-input"
+                onChange={(n) => console.log(n.target.value)}
+              />
+            </div>
+
+            <div className="profile-input-with-header">
+              <h3>Last Name</h3>
+              <input
+                type="text"
+                className="profile-input"
+                onChange={(n) => console.log(n.target.value)}
+              />
+            </div>
           </div>
 
-          <img src={user.photoURL} />
+          <img
+            src={user?.photoURL}
+            alt="User Img"
+            className="profile-picture"
+          />
+        </div>
+
+        <div className="profile-goals-section">
+          <div className="profile-goals">
+            <h2>Goals</h2>
+
+            <div className="profile-goals-container">
+              {user === null ? (
+                <Loading />
+              ) : (
+                <Goals goals={goals} editable={true} />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
